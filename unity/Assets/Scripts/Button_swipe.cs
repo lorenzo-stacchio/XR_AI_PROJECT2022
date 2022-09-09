8aa52bf4d6424018c1c4e1ba539d833b0341b3c0
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2022-09-09 17:31:20 +0200

    load some models and ui

--- unity/Assets/Scripts/Button_swipe.cs
+++ unity/Assets/Scripts/Button_swipe.cs
@@ -10,22 +10,38 @@ public class Button_swipe : MonoBehaviour
 
     private int elements;
     private int actual;
+    public GameObject manager_owner; 
+    private Model_manager manager;
 
     void Start(){
-        this.elements = 3;
         this.actual = 0;
+        Debug.Log("Manager" + this.manager_owner);
+
+
     }
 
     public void forward(){
+        Debug.Log("Manager" + this.manager_owner);
+
+        this.manager = this.manager_owner.GetComponent<Model_manager>();
+        this.elements = this.manager.getnumchild();
+
         this.actual = (this.actual +1) % this.elements;
+        this.manager.Set_active_model(this.actual);
+        //forward to Model Manager
     }
 
 
     public void backward(){
+        Debug.Log("Manager" + this.manager_owner);
+        this.manager = this.manager_owner.GetComponent<Model_manager>();
+        this.elements = this.manager.getnumchild();
+
         this.actual = (this.actual - 1);
         if (this.actual < 0){
             this.actual = this.elements -1;
         }
+        this.manager.Set_active_model(this.actual);
     }
 
 
---
+++ unity/Assets/Scripts/Model_manager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Model_manager : MonoBehaviour
+{
+
+    List<GameObject> model_names;
+
+    int active_index;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.model_names = new List<GameObject>();
+        //take all gameobject in child
+        foreach (Transform child in transform)
+        {
+            Debug.Log("child" + child );
+            this.model_names.Add(child.gameObject);
+        }
+        Debug.Log("num of 3d models" + this.model_names.Count);
+
+        // activate first
+        this.active_index = 0;
+        this.model_names[active_index].SetActive(true);
+
+    }
+
+    public void Set_active_model(int index){
+        this.model_names[this.active_index].SetActive(false);
+
+        //set active
+        this.active_index = index;
+        this.model_names[this.active_index].SetActive(true);
+    }
+
+    public int getnumchild(){
+        return  this.model_names.Count;
+    }
+
+}
